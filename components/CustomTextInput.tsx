1fa6c30113714c9b7ff262f6da6c595d9b0bf4b0
Satvik24511 <satvik24511@iiitd.ac.in>
2025-06-28 12:18:09 +0530

    added custom text input

---
+++ components/CustomTextInput.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { TextInput, StyleSheet, View, Text, TextInputProps, TextStyle, ViewStyle } from 'react-native';
+
+interface CustomTextInputProps extends TextInputProps {
+  label?: string;
+  error?: string;
+}
+
+export const CustomTextInput: React.FC<CustomTextInputProps> = ({ label, error, style, ...props }) => {
+  return (
+    <View style={customTextInputStyles.container}>
+      {label && <Text style={customTextInputStyles.label}>{label}</Text>}
+      <TextInput
+        style={[customTextInputStyles.input, style, error ? customTextInputStyles.inputError : null]}
+        placeholderTextColor="#999"
+        {...props}
+      />
+      {error && <Text style={customTextInputStyles.errorText}>{error}</Text>}
+    </View>
+  );
+};
+
+const customTextInputStyles = StyleSheet.create({
+  container: {
+    marginBottom: 15,
+    width: '100%',
+  },
+  label: {
+    fontSize: 16,
+    color: '#333',
+    marginBottom: 8,
+    fontWeight: '600',
+  },
+  input: {
+    height: 50,
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    fontSize: 16,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+  inputError: {
+    borderColor: '#ff6161',
+    borderWidth: 2,
+  },
+  errorText: {
+    color: '#ff6161',
+    fontSize: 12,
+    marginTop: 5,
+  },
+});
